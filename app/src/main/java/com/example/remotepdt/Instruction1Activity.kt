aa148063192b05711651bf3e4e5907065f91f436
Shreya Setlur <shreyasetlur18.com>
2024-11-07 12:21:42 -0500

    Add instruction activities and layouts

---
+++ app/src/main/java/com/example/remotepdt/Instruction1Activity.kt
@@ -0,0 +1,23 @@
+package com.example.remotepdt
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class Instruction1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_instruction1)
+
+        // Find the Next button by its ID
+        val btnNext = findViewById<Button>(R.id.btnNext)
+
+        // Set an OnClickListener on the Next button
+        btnNext.setOnClickListener {
+            // Start Instruction2Activity when the button is clicked
+            val intent = Intent(this, Instruction2Activity::class.java)
+            startActivity(intent)
+        }
+    }
+}
