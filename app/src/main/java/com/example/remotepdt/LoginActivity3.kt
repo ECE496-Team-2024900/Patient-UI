269fd21bde60a6bbb5d0bc7f1da928627205611f
faatima_abidi <faatima.abidi24@gmail.com>
2024-12-24 21:05:15 -0500

    components added to signup page

--- app/src/main/java/com/example/remotepdt/LoginActivity3.kt
+++ app/src/main/java/com/example/remotepdt/LoginActivity3.kt
@@ -1,11 +1,14 @@
 package com.example.remotepdt
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_part_3)
+        val emailInput = findViewById<EditText>(R.id.emailInput)
+        emailInput.hint = intent.getStringExtra("email")
     }
 }
