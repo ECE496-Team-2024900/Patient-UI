8b0ddf5a64368a1bce668fe571affca543f3df36
faatima_abidi <faatima.abidi24@gmail.com>
2024-12-24 20:36:49 -0500

    First page done

--- app/src/main/java/com/example/remotepdt/LoginActivity.kt
+++ app/src/main/java/com/example/remotepdt/LoginActivity.kt
@@ -3,22 +3,52 @@ package com.example.remotepdt
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
+import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
+    private var BeUrl = "http://10.0.2.2:8000"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         // Find the Login button by its ID
         val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val emailInput = findViewById<EditText>(R.id.emailInput)
 
         // Set an OnClickListener on the Login button
         btnLogin.setOnClickListener {
-            // Start TreatmentSessionActivity when the button is clicked
-            val intent = Intent(this, WelcomeActivity::class.java)
-            intent.putExtra("email", "mickey.mouse@disney.org") //use placeholder email for now
-            startActivity(intent)
+            val email = emailInput.getText().toString()
+            AndroidNetworking.get("${BeUrl}/user_auth/check_if_user_exists")
+                .addQueryParameter("email", email)
+                .build()
+                .getAsJSONObject(object : JSONObjectRequestListener {
+                    override fun onResponse(response: JSONObject?) {
+                        if (response?.optString("message") == "User does not exist") {
+                            val intent = Intent(this@LoginActivity, LoginActivity3::class.java)
+                            intent.putExtra("email", email)
+                            startActivity(intent)
+                        } else {
+                            val intent = Intent(this@LoginActivity, LoginActivity2::class.java)
+                            intent.putExtra("email", email)
+                            startActivity(intent)
+                        }
+                    }
+                    override fun onError(anError: ANError) {
+                            // Handle error
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "Error: ${anError.message}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                    }
+                })
         }
     }
 }
---
+++ app/src/main/java/com/example/remotepdt/LoginActivity3.kt
@@ -0,0 +1,11 @@
+package com.example.remotepdt
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_part_3)
+    }
+}
