a575082af10262ffc4bfec9fc67c50e0116548f9
zoya5636 <zoya@chishtie.com>
2025-03-14 18:48:25 -0400

    Cleaned up again

--- app/src/main/java/com/example/remotepdt/MainApplication.kt
+++ app/src/main/java/com/example/remotepdt/MainApplication.kt
@@ -9,42 +9,8 @@ import live.videosdk.rtc.android.VideoSDK
 import org.json.JSONObject
 
 class MainApplication: Application() {
-    private var BeUrl = "http://10.0.2.2:8002"
-
     override fun onCreate() {
         super.onCreate()
         VideoSDK.initialize(applicationContext)
-
-        // Establishing connection
-//        val bluetoothComm = BluetoothComm.getInstance(applicationContext)
-//
-//        // Replace fields with the actual serial number
-//        bluetoothComm.connect("1234")
-
-        // Getting serial number for this patient
-//        AndroidNetworking.get("${BeUrl}/users/get_patient_info")
-//            .addQueryParameter("email", "mickey.mouse@disney.org")
-//            .addQueryParameter("fields", "medical_device_id")
-//            .build()
-//            .getAsJSONObject(object : JSONObjectRequestListener {
-//                override fun onResponse(response: JSONObject) {
-//                    val serialNumber = response.optString("medical_device_id").toString()
-//
-//                    // Establishing connection
-//                    val bluetoothComm = BluetoothComm.getInstance(applicationContext)
-//
-//                    // Replace fields with the actual serial number
-//                    bluetoothComm.connect(serialNumber)
-//                }
-//
-//                override fun onError(anError: ANError) {
-//                    anError.printStackTrace()
-//                    val errorMessage = anError.message ?: "An error occurred retrieving the medical device serial number."
-//                    Toast.makeText(
-//                        this@MainApplication, errorMessage,
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                }
-//            })
     }
 }
