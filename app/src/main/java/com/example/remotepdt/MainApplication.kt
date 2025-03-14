39bc53459fce813b2cdabf21822e3348142ab5fc
zoya5636 <zoya@chishtie.com>
2025-03-14 18:48:55 -0400

    removed unused imports

--- app/src/main/java/com/example/remotepdt/MainApplication.kt
+++ app/src/main/java/com/example/remotepdt/MainApplication.kt
@@ -1,12 +1,7 @@
 package com.example.remotepdt
 
 import android.app.Application
-import android.widget.Toast
-import com.androidnetworking.AndroidNetworking
-import com.androidnetworking.error.ANError
-import com.androidnetworking.interfaces.JSONObjectRequestListener
 import live.videosdk.rtc.android.VideoSDK
-import org.json.JSONObject
 
 class MainApplication: Application() {
     override fun onCreate() {
